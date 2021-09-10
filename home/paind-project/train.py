f87e4f1d42fbc411fcdee2cb291b138553956d14
Aditya Mittal <39227064+adityamittal13@users.noreply.github.com>
2021-09-10 06:39:49 -0700

    Update train.py

--- home/paind-project/train.py
+++ home/paind-project/train.py
@@ -2,7 +2,7 @@
 import torch
 from torch import nn
 from torch import optim
-from torchvision import transforms, models, datasets 
+from torchvision import transforms, models, datasets  
 
 import numpy as np
 import matplotlib.pyplot as plt
